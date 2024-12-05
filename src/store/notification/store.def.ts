52f5f79d1bd72965b8dbc750008c0b72ccb3eb34
khaitbek <khaitbek2005@gmail.com>
2024-12-05 14:03:35 +0500

    disable devtools

--- src/store/notification/store.def.ts
+++
@@ -1,8 +0,0 @@
-import type { Notification } from "@/core/entities/notification.entity";
-
-export interface NotificationStoreDef {
-  notifications: Notification[];
-  setNotifications: (notifications: Notification[]) => void;
-  shouldRefetchNotifications: boolean;
-  setShouldRefetchNotifications: (shouldRefetchNotifications: boolean) => void;
-}
